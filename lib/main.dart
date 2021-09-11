0bf4c0ff6e875f66158d45dd377690a59e8667f3
nisaefendioglu <nisaefendioglu0@gmail.com>
2021-09-11 11:22:55 +0300

    edited.

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-          ),
-        )),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -42,7 +34,7 @@ class StartPage extends StatelessWidget {
                   style: TextStyle(
                       fontSize: 40,
                       height: 2.0,
-                      color: Colors.black,
+                      color: Color.fromRGBO(53, 50, 62, 10),
                       fontWeight: FontWeight.bold),
                 ),
               ),
@@ -54,45 +46,65 @@ class StartPage extends StatelessWidget {
                   "Let's get started!",
                   style: TextStyle(
                       fontSize: 20,
-                      color: Colors.black,
+                      color: Color.fromRGBO(53, 50, 62, 10),
                       fontWeight: FontWeight.bold),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-                child: Center(
-                  child:
-                      Text("Paste your first link into the field to shorten it",
-                          style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.black,
-                          )),
+              Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                        "Paste your first link into the field to shorten it",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Color.fromRGBO(76, 74, 85, 10),
+                            fontWeight: FontWeight.bold)),
+                  ),
                 ),
               ),
-              SizedBox(
-                height: 80,
-                width: 80,
-                child: TextField(
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      contentPadding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      border: OutlineInputBorder(),
-                      hintText: 'Shorten a link here ...'),
+              Center(
+                child: SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(10.0),
+                          ),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Colors.white,
+                        filled: true,
+                        hintText: 'Shorten a link here ...'),
+                  ),
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  print("Button Click");
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0)),
-                  minimumSize: Size(50, 40),
+              Center(
+                child: SizedBox(
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      print("Button Click");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      minimumSize: Size(60, 40),
+                    ),
+                    child: Text('SHORTEN IT!'),
+                  ),
                 ),
-                child: Text('SHORTEN IT!'),
               )
             ],
           ),
